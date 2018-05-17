5d8b3511f7324510a836d5fdb406040fd749a797
Paolo <ppiron@gmail.com>
2018-05-17 21:02:54 +0100

    all divs done

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ class App extends Component {
     this.state = {
       sessionLength: "25:00",
       breakLength: "5:00",
+      timerLabel: "Session",
+      timeLeft: "25:00",
+      startStop: true,
     };
 
     this.handleClick = this.handleClick.bind(this);
@@ -16,8 +19,6 @@ class App extends Component {
   handleClick(event) {
     
   }
-
-  
 
   render() {
     
@@ -29,8 +30,16 @@ class App extends Component {
         <div id="pomodoro">
           <div id="break-label">Break Length</div>
           <div id="session-label">Session Length</div>
-          <div id="break-length">{state.breakLength}</div>
-          <div id="session-length">{state.sessionLength}</div>
+          <div id="break-length">{this.state.breakLength}</div>
+          <div id="session-length">{this.state.sessionLength}</div>
+          <div id="break-decrement">-</div>
+          <div id="session-decrement">-</div>
+          <div id="break-increment">+</div>
+          <div id="session-increment">+</div>
+          <div id="timer-label">{this.state.timerLabel}</div>
+          <div id="time-left">{this.state.timeLeft}</div>
+          <div id="start-stop">{this.state.startStop ? "Start" : "Stop"}</div>
+          <div id="reset">Reset</div>
         </div>
         
       </div>
